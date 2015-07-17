31d0b4565f003a51168b5f948dd585ca1042d07a
Mauro Gonzalez <mauro.d.gonzalez90@gmail.com>
2015-07-17 16:44:22 -0300

    se agrega javascript

---
+++ mobile_wall/mobile_wall/OnLoadEvent.js
@@ -0,0 +1,11 @@
+var body = document.getElementsByTagName("body")[0];
+
+body.addEventListener("load", load(), false);
+
+function init() {
+    alert("it works!");
+};
+
+function load() {
+    location.href = "tnqawall:\\callfunction/asd";
+};
